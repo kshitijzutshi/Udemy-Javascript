24e7971f557a336d7cb02778102f0ec9b51c01f2
kshitij zutshi <kshitij.zutshi@gmail.com>
2019-12-23 16:26:35 +0530

    JS-Basics

---
+++ script.js
@@ -0,0 +1,16 @@
+// Variables and Data types
+
+// https://developers.google.com/web/tools/chrome-devtools/javascript/snippets
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
