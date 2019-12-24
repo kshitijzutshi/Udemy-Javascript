7f9df519deef0607ea9fb689f60e5ca25c8697f3
kshitij zutshi <kshitij.zutshi@gmail.com>
2019-12-24 11:19:49 +0530

    Variable mutation and coercion

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -14,3 +14,23 @@ console.log(fullAge);
 var job;
 console.log(job);
 
+// Variables mutation and type coercion
+
+
+// type coercion Example
+var firstName = 'John';
+var age = 28;
+console.log(firstName + ' ' + age);
+
+var job, isMarried ;
+
+job = 'teacher';
+isMarried = false
+console.log(firstName +' is a '+job + ' and is he married? ' + isMarried);
+
+// Variable mutation
+// age that was integer is now a string
+age = 'twenty eight';
+// alert(age);
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
