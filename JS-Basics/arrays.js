67ce59c9a24cdbb9ed5435b9941b1f5926edf421
kshitijzutshi <kshitij.zutshi@gmail.com>
2019-12-28 18:34:37 +0530

    arrays concepts

---
+++ JS-Basics/arrays.js
@@ -0,0 +1,16 @@
+/*****************************
+ * Arrays
+ * 
+ */
+
+ var names = ['John','Mark','Jane'];
+ var years = new Array(1990,1969,1948);
+
+ console.log(names[0]);
+ console.log(names.length);
+
+ // Mutate the data in array
+
+ names[1] = 'Ben';
+ names.pop();
+ console.log(names[5]);
