27233336ccc41f37ab8ba24d4531fa263ca4a2f2
kshitijzutshi <kshitij.zutshi@gmail.com>
2019-12-28 22:11:25 +0530

    changes to arrays.js

--- JS-Basics/arrays.js
+++ JS-Basics/arrays.js
@@ -6,11 +6,20 @@
  var names = ['John','Mark','Jane'];
  var years = new Array(1990,1969,1948);
 
- console.log(names[0]);
+ names[5] = 'KZEE';
+ console.log(names);
  console.log(names.length);
+ names[1] = 'Ben';
+ names.pop();
+ names.shift();
+ names.unshift('bata');
+ console.log('---------------------------------------------------------');
+ console.log(names);
+ console.log(names.length);
+//  console.log('5th name is '+names[5]);
 
  // Mutate the data in array
 
- names[1] = 'Ben';
- names.pop();
- console.log(names[5]);
+//  names[1] = 'Ben';
+//  names.pop();
+//  console.log(names[5]);
