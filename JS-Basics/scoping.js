68197debaa3bd78461f0489dd1d357844c00b23a
kshitij zutshi <kshitij.zutshi@gmail.com>
2020-01-01 15:16:33 +0530

    Execution stack vs scope chain lexical example

--- JS-Basics/scoping.js
+++ JS-Basics/scoping.js
@@ -13,3 +13,26 @@ function first(){
         console.log(a+b+c);
     }
 }
+
+// Example to show the difference between Execution stack and scope chain
+
+var a = "Hello";
+first();
+
+function first(){
+    var b = "Hey!";
+    second();
+
+    function second(){
+        var c = "Hi";
+        // console.log(a+b+c);
+        third();
+    }
+}
+
+function third(){
+    var d = "John";
+    // console.log(c); // Error : scoping.js:35 Uncaught ReferenceError: c is not defined
+    console.log(d+a); // As a and d in global execution context its visible.
+}
+    
