b39ddae86fb948fa1530cabafd350644d475a9f5
kshitij zutshi <kshitij.zutshi@gmail.com>
2020-01-08 12:30:58 +0530

    Update app.js

--- DOM-Pig-game/app.js
+++ DOM-Pig-game/app.js
@@ -106,7 +106,7 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
   roundScore = 0;
 
   // Check if the player has WON
-  if (scores[activePlayer] >= 20) {
+  if (scores[activePlayer] >= 100) {
     document.querySelector("#name-" + activePlayer).textContent = "WINNER!";
     document.querySelector(".dice").style.display = "none";
     // Add CSS to winner of player
