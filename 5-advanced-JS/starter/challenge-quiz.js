05ff9047bff59b80a30a0b3013d23e72e8ec3a6e
kshitijzutshi <kshitij.zutshi@gmail.com>
2020-05-16 17:45:19 +0530

    challenge complete

--- 5-advanced-JS/starter/challenge-quiz.js
+++ 5-advanced-JS/starter/challenge-quiz.js
@@ -32,35 +32,68 @@
     }
   };
 
-  Question.prototype.checkAns = function (ans) {
+  Question.prototype.checkAnswer = function (ans, callback) {
+    var sc;
+
     if (ans === this.correct) {
-      console.log("Correct answer!!");
+      console.log("Correct answer!");
+      sc = callback(true);
     } else {
-      console.log("Wrong answer!!");
+      console.log("Wrong answer. Try again :)");
+      sc = callback(false);
     }
+
+    this.displayScore(sc);
+  };
+
+  Question.prototype.displayScore = function (score) {
+    console.log("Your current score is: " + score);
+    console.log("------------------------------");
   };
 
   var q1 = new Question(
-    "Is javascript the best programming language in the world?",
+    "Is JavaScript the coolest programming language in the world?",
     ["Yes", "No"],
     0
   );
 
-  var q2 = new Question("Have you worked with Java before?", ["Yes", "No"], 0);
+  var q2 = new Question(
+    "What is the name of this course's teacher?",
+    ["John", "Micheal", "Jonas"],
+    2
+  );
 
   var q3 = new Question(
-    "What does best decribe coding?",
-    ["Boring", "Hard", "Fun"],
+    "What does best describe coding?",
+    ["Boring", "Hard", "Fun", "Tediuos"],
     2
   );
 
   var questions = [q1, q2, q3];
 
-  var num = Math.floor(Math.random() * questions.length);
+  function score() {
+    var sc = 0;
+    return function (correct) {
+      if (correct) {
+        sc++;
+      }
+      return sc;
+    };
+  }
+  var keepScore = score();
 
-  questions[num].displayQuestion();
+  function nextQuestion() {
+    var n = Math.floor(Math.random() * questions.length);
+    questions[n].displayQuestion();
 
-  var ans = parseInt(prompt("Please select the correct answer."));
+    var answer = prompt("Please select the correct answer.");
 
-  questions[num].checkAns(ans);
+    if (answer !== "exit") {
+      questions[n].checkAnswer(parseInt(answer), keepScore);
+
+      nextQuestion();
+    }
+  }
+
+  nextQuestion();
 })();
